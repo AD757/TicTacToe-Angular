3d6d2227d8c74836e467c265839243173b207c06
ad757 <alladin.daher@hotmail.com>
2023-01-21 23:55:25 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# TicTacToe
+
+Beginner project to practice Angular.
+
+## Link
+
+- [Tic Tac Toe](https://tictactoe-ad757.vercel.app/)
+
+### Screen
+
+![Alt text](img/tictactoe.png "screenshot")
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule, isDevMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SquareComponent,
+    BoardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
